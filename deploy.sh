529d0c637256d130e755f28a43b362188b64ea7c
Sharib Zafar <sharib.zafar4@gmail,com>
2020-01-21 22:52:38 +0530

    added deploy.sh and modified .travis.yaml

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+docker build -t sharibzafar4/multi-client:latest -t sharibzafar4/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t sharibzafar4/multi-server:latest -t sharibzafar4/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t sharibzafar4/multi-worker:latest -t sharibzafar4/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push sharibzafar4/multi-client:latest
+docker push sharibzafar4/multi-server:latest
+docker push sharibzafar4/multi-worker:latest
+
+docker push sharibzafar4/multi-client:$SHA
+docker push sharibzafar4/multi-server:$SHA
+docker push sharibzafar4/multi-worker:$SHA
+
+kubectl apply -f k8s
+
+kubectl set image Deployment:server-deployment server=sharibzafar4/multi-server:$SHA
+kubectl set image Deployment:client-deployment client=sharibzafar4/multi-client:$SHA
+kubectl set image Deployment:worker-deployment worker=sharibzafar4/multi-worker:$SHA
